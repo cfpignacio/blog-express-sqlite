19428180adfb301e12730e615a34679afc6c9e71
Ignacio Camillucci <66032700+cfpignacio@users.noreply.github.com>
2023-08-29 19:05:25 -0300

    add db

--- server.ts
+++ server.ts
@@ -1,19 +1,30 @@
 import express, { Express, Request, Response } from 'express';
 import bodyParser from 'body-parser';
-import noticiasRoutes from './modules/notcias/notica.routes';
+import noticiasRoutes from './modules/noticias/notica.routes';
+import { dbcontext } from './modules/db/dbcontext';
+
+dbcontext
+	.initialize()
+	.then(() => {
+		console.log('Base datos OK');
+	})
+	.catch((err) => {
+		console.error('Base datos DOWN', err);
+	});
 
 const app: Express = express();
 
 app.use(bodyParser.json());
 
-app.use('/', (req: Request, res: Response) => {
+app.use('/', async (req: Request, res: Response) => {
 	res.json({
 		mgs: 'Servidor funcionando OK 🚀🚀',
 	});
+	res.status(201).json({ success: true });
 });
 
 app.use('/noticia', noticiasRoutes);
 
 app.listen(3000, () => {
-	console.log('Servidor funcionando OK 🚀🚀 EN EL PORT 3000');
+	console.log('Servidor funcionando OK 🚀 EN EL PORT 3000');
 });
